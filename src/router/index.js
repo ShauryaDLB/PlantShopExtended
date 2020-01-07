2812eddc007598bc8e90b7a73811d2673d1d8f56
Arvid Strauss <avst@mms-t-systems.de>
2020-01-07 11:31:10 +0100

    added all views and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
+import Product from "../views/Product.vue";
+import Login from "../views/Login.vue";
+import Forum from "../views/Forum.vue";
+import Success from "../views/Success.vue";
 import i18n from "@/i18n";
 
 Vue.use(VueRouter);
@@ -25,9 +28,24 @@ const routes = [
         component: Home
       },
       {
-        path: "about",
-        name: "about",
-        component: About
+        path: "product",
+        name: "product",
+        component: Product
+      },
+      {
+        path: "login",
+        name: "login",
+        component: Login
+      },
+      {
+        path: "forum",
+        name: "forum",
+        component: Forum
+      },
+      {
+        path: "success",
+        name: "success",
+        component: Success
       }
     ]
   }
