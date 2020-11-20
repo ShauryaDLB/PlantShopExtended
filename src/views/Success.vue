8a45af4db8332acb387dc3914b07abfc17e87e77
ShauryaDLB <shaurya.v23off@gmail.com>
2020-11-20 09:32:17 +0100

    Succuess page changed to look better

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="success">
-    <div class="jumbotron text-center">
+    <div class="jumbotron text-center" id="suc">
       <h1 class="display-3">Vielen Dank für Ihren Einkauf!</h1>
       <p class="lead">
         <strong>Überprüfen Sie Ihren E-Mail Ordner</strong> für weitere Details.
@@ -15,6 +15,19 @@
           >Zurück zur Startseite</router-link
         >
       </p>
+      <CarouselSlider />
     </div>
   </div>
 </template>
+<script>
+import CarouselSlider from "@/components/CarouselSlider.vue";
+export default {
+  name: "Success",
+  components:{
+  CarouselSlider
+  }
+}
+</script>
+<style scoped>
+
+</style>
