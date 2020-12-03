51698e1813f9702b607c35dcfdfcccdab6cbfd16
ShauryaDLB <shaurya.v23off@gmail.com>
2020-12-03 01:35:46 +0100

    Made success page look much better

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="success">
     <div class="jumbotron text-center" id="suc">
+      <div class="imgg">
+      <img src="@/assets/img/cart_.jpg" class=""  height="250" width="250" />
+      </div>
       <h1 class="display-3">Vielen Dank für Ihren Einkauf!</h1>
       <p class="lead">
         <strong>Überprüfen Sie Ihren E-Mail Ordner</strong> für weitere Details.
@@ -15,19 +18,17 @@
           >Zurück zur Startseite</router-link
         >
       </p>
-      <CarouselSlider />
     </div>
   </div>
 </template>
 <script>
-import CarouselSlider from "@/components/CarouselSlider.vue";
-export default {
-  name: "Success",
-  components:{
-  CarouselSlider
-  }
-}
+export default {};
 </script>
 <style scoped>
-
+.imgg {
+}
+#suc{
+  background-color: white;
+  background-image: url('../assets/img/confetti.png');
+}
 </style>
