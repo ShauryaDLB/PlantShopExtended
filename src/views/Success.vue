770ce46848dcefa5306e380824c6043f0b9e1814
ShauryaDLB <shaurya.v23off@gmail.com>
2020-12-08 13:51:53 +0100

    Added order summary to success page

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -1,34 +1,108 @@
+<script>
+window.onload = function(){
+  var a = JSON.parse(window.localStorage.getItem('vuex'));
+  var b = a["cartProducts"];
+  var i;
+  var sum =0;
+  for (i = 0; i < b.length; i++){
+    document.getElementById('prod' + i).innerHTML = b[i].title;
+    document.getElementById('prodqty' + i).innerHTML = b[i].qty;
+    document.getElementById('prodprice' + i).innerHTML ='€ ' + b[i].price;
+    sum += b[i].price;
+  }
+  document.getElementById('totalPrice').innerHTML ='€ ' + Math.round(sum);
+}
+</script>
 <template>
   <div class="success">
     <div class="jumbotron text-center" id="suc">
       <div class="imgg">
       <img src="@/assets/img/cart_.jpg" class=""  height="250" width="250" />
       </div>
+        <h2><b>Order Summary</b></h2>
+
+<table class="cen">
+  <tr >
+    <th>PRODUCT</th>
+    <th>QUANTITY</th>
+    <th>PRICE</th>
+  </tr>
+  <tr>
+    <td id="prod0"></td>
+    <td id="prodqty0"></td>
+    <td id="prodprice0"></td>
+  </tr>
+  <tr>
+    <td id="prod1"></td>
+    <td id="prodqty1"></td>
+    <td id="prodprice1"></td>
+  </tr>
+  <tr>
+    <td id="prod2"></td>
+    <td id="prodqty2"></td>
+    <td id="prodprice2"></td>
+  </tr>
+    <tr>
+    <td id="prod3"></td>
+    <td id="prodqty3"></td>
+    <td id="prodprice3"></td>
+  </tr>
+  <tr>
+    <td>TOTAL</td>
+    <td> - </td>
+    <td id="totalPrice"></td>
+</tr>
+</table>
       <h1 class="display-3">Vielen Dank für Ihren Einkauf!</h1>
       <p class="lead">
         <strong>Überprüfen Sie Ihren E-Mail Ordner</strong> für weitere Details.
       </p>
       <hr />
-      <p>Haben Sie Probleme? <a href="">Kontaktieren Sie uns</a></p>
+      <p>Haben Sie Probleme? <a id="coll" href="">Kontaktieren Sie uns</a></p>
       <p class="lead">
         <router-link
           class="btn btn-primary"
           :to="'/' + $i18n.locale"
           role="button"
+          id="col"
           >Zurück zur Startseite</router-link
         >
       </p>
+      
     </div>
   </div>
 </template>
-<script>
-export default {};
-</script>
 <style scoped>
 .imgg {
+}
+#col{
+  background-color: #1BADA2;
+}
+#coll{
+  text-emphasis-color: #1BADA2;
 }
 #suc{
   background-color: white;
   background-image: url('../assets/img/confetti.png');
+}
+.cen {
+  margin-left: auto;
+  margin-right: auto;
+}
+table {
+  border-collapse: collapse;
+  width: 40%;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr{background-color: #ffffff}
+
+th {
+  background-color: #1BADA2;
+  color: white;
 }
 </style>
